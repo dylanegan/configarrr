7122e80ee276ca37b11953dc480b2951c6783e4b
Dylan Egan <me@dylanegan.com>
2009-10-12 20:54:11 +1100

    Start adding yardoc support.

--- lib/configarrr/base.rb
+++ lib/configarrr/base.rb
@@ -1,20 +1,36 @@
 module Configarrr
   class Base
+    # Initialise a new Configarrr::Base object
+    #
+    # @param [Hash] options the options to be parsed
+    # @return [Configarrr::Base] self
     def initialize(options = {})
       @keys = []
       parse_options(options)
     end
 
+    # Helper method to configure a new object and return it
+    #
+    # @param [Hash] options the options to be parsed
+    # @param [Proc] block the block to be evaluated
+    # @return [Configarrr::Base] the initialised configarrr object
     def self.configure(options = {}, &block)
       config = new(options)
       config.configure(&block)
       config
     end
 
+    # Allow a block to be passed and evaluated
+    #
+    # @param [Proc] block the block to be evaluated
     def configure(&block)
       instance_eval(&block)
     end
 
+    # Sets the key to the value specified and returns self
+    #
+    # @param [Hash, String, Symbol] key the name of settings key
+    # @param [Object] value the value of the settings key
     def set(key, value=self)
       if value.kind_of?(Proc)
         metadef(key, &value)
@@ -32,6 +48,9 @@ module Configarrr
       self
     end
 
+    # Returns a hash of all the set keys and their values
+    #
+    # @return [Hash] a hash of the keys and values
     def to_hash
       @keys.inject({}) do |hash, key|
         hash[key] = __send__ "#{key}"
--- lib/configarrr/yaml.rb
+++ lib/configarrr/yaml.rb
@@ -2,6 +2,10 @@ require 'yaml'
 
 module Configarrr
   class YAML < Base
+    # Initialise a new Configarrr::YAML object
+    #
+    # @param [Hash] options the options to be parsed
+    # @return [Configarrr::YAML] self
     def initialize(options = {})
       raise ArgumentError, "Please provide a YAML file location." unless options[:file]
       super(options)
@@ -10,10 +14,14 @@ module Configarrr
       set_yaml
     end
 
+    # An overridable method to provide default settings for YAML
+    #
+    # @return [Hash] a hash of default key and value pairings
     def defaults
       {}
     end
 
+    # Take the YAML and set it
     def set_yaml
       if @parent
         @yaml.has_key?(@parent) ? set(defaults.recursive_merge(@yaml[@parent])) : raise(Configarrr::OptionError, "Please provide a valid parent value. #{@parent} does not exist.")
@@ -22,10 +30,14 @@ module Configarrr
       end
     end
 
+    # Get the key and value pairings and save it to the file
     def save
       ::File.open(@file, "w") { |file| file << self.to_hash.to_yaml }
     end
 
+    # Ensure that if a YAML parent is provided that it is merged back in to the parsed YAML
+    #
+    # @return [Hash] a hash of the key and value pairings
     def to_hash
       @parent ? @yaml.merge({@parent => super}) : super
     end
